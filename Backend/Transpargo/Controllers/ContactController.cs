33e325807db3aa75ea101f261edc9b1b2cd74f6f
Teepakraaj <gteepak2003@gmail.com>
2025-12-29 14:30:03 +0530

    final

---
+++ Backend/Transpargo/Controllers/ContactController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using Transpargo.Models;
+
+[ApiController]
+[Route("api/contact")]
+public class ContactController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult SendMail([FromBody] ContactRequest request)
+    {
+        try
+        {
+            var smtp = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(
+                    "shippingagencyspartan@gmail.com",
+                    "jbbj zqhu exzr yqjx"
+                ),
+                EnableSsl = true
+            };
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress("shippingagencyspartan@gmail.com"),
+                Subject = "New Contact Us Message",
+                Body = $@"
+Name: {request.Name}
+Email: {request.Email}
+
+Message:
+{request.Message}
+"
+            };
+
+            // Send TO shipping agency
+            mail.To.Add("shippingagencyspartan@gmail.com");
+
+            // IMPORTANT: allow reply directly to user
+            mail.ReplyToList.Add(new MailAddress(request.Email));
+
+            smtp.Send(mail);
+
+            return Ok(new { success = true });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                success = false,
+                error = ex.Message
+            });
+        }
+    }
+}
